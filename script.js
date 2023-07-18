0079152194a74cf563fb780deee2816ad5295447
qu-js <fsung2896@gmail.com>
2023-07-18 23:10:04 +0900

    almostdone

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 var contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+document.getElementById("save_card").addEventListener("click", ()=>{
+    addFlashcard();
+});
+
+document.getElementById("delete_cards").addEventListener("click", ()=>{
+    localStorage.clear();
+    flashcards.innerHTML = '';
+    contentArray = [];
+});
+
+
+
+
+document.getElementById("show_card_box").addEventListener("click", ()=>{
+    document.getElementById("create_card").style.display = "block";
+});
+
+document.getElementById("close_card_box").addEventListener("click", ()=>{
+    document.getElementById("create_card").style.display = "none";
+});
+
+
+
